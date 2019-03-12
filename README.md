8c270d3c851c2185831ac9444ca5c75e60bcc264
Gabriel Lamas <gabriel.rosa@cbyk.com.br>
2019-03-12 15:16:52 -0300

    NPM, webpack
    Add npm and webpack

---
+++ README.md
@@ -0,0 +1,39 @@
+# Gamers Club Test
+
+Gabriel Lamas
+
+## How to use
+
+Open project on terminal, and choose one command:
+
+To edit project 
+
+```html
+npm run dev
+```
+
+To build project for production
+
+```html
+npm run build
+```
+
+## To do
+
+- [ ] Add form validation
+
+## Contributing
+
+Please read [CONTRIBUTING.md](CONTRIBUTING.md) for details on our code of conduct, and the process for submitting pull requests to us.
+
+## Versioning
+
+We use [SemVer](http://semver.org/) for versioning. For the versions available, see the [tags on this repository](https://github.com/gabriellamas/Fancy-Checkout/tags).
+
+## Authors
+
+See also the list of [contributors](https://github.com/gabriellamas/Fancy-Checkout/contributors) who participated in this project.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
--- js/index.js
+++ js/index.js
@@ -1,7 +1,6 @@
 (function () {
     'use strict'
 
-
     fetch('https://api.myjson.com/bins/sqwaq', {
         method: 'GET'
     }).then(response => {
@@ -12,13 +11,15 @@
         console.error(err);
     });
 
-
     function buildScreen(data) {
         buildHeader(data)
         buildForFunArea(data)
+        buildLobbyAndRankedArea(data)
+        buildfooter(data)
     }
 
     function buildHeader(data) {
+        console.log(data)
 
         const userName = document.querySelector('.gc-name');
         const userId = document.querySelector('.gc-id');
@@ -29,31 +30,43 @@
         let pinLevelAmateur = document.querySelector('.pin-level-amateur')
         let pinLevelPro = document.querySelector('.pin-level-pro')
 
+        let featuredMedal = document.querySelector('.-featured-medal')
+        let position = document.querySelector('.-position')
+        let patent = document.querySelector('.-patent')
+        let level = document.querySelector('.-level')
+        //let subscription = document.querySelector('.-subscription')
+
         userName.textContent = `GC.${data.user.name}`
         userId.textContent = `GC ID: ${data.user.id}`;
+
+        featuredMedal.src = data.user.featured_medal.image;
+        position.src = data.user.game_position.image;
+        patent.src = data.user.patent;
+        level.textContent = data.user.level;
         
-        if(data.user.expertise == 'casual'){
+
+        if (data.user.expertise == 'casual') {
             progressBar.style.cssText = "width:25%;"
             pinLevelCasual.src = 'img/pin-level-blue.png'
 
-        }else if(data.user.expertise == 'competitive'){
+        } else if (data.user.expertise == 'competitive') {
             progressBar.style.cssText = "width:50%;"
             pinLevelCasual.src = 'img/pin-level-blue.png'
             pinLevelCOmpetitive.src = 'img/pin-level-blue.png'
 
-        }else if(data.user.expertise == 'amateur'){
+        } else if (data.user.expertise == 'amateur') {
             progressBar.style.cssText = "width:75%;"
             pinLevelCasual.src = 'img/pin-level-blue.png'
             pinLevelCOmpetitive.src = 'img/pin-level-blue.png'
             pinLevelAmateur.src = 'img/pin-level-blue.png'
 
-        }else if(data.user.expertise == 'pro'){
+        } else if (data.user.expertise == 'pro') {
             progressBar.style.cssText = "width:100%;"
             pinLevelCasual.src = 'img/pin-level-blue.png'
             pinLevelCOmpetitive.src = 'img/pin-level-blue.png'
             pinLevelAmateur.src = 'img/pin-level-blue.png'
             pinLevelPro.src = 'img/pin-level-blue.png'
-        }
+        }      
 
     }
 
@@ -70,12 +83,86 @@
     }
 
     function buildServersOptions(data) {
-        console.log(data)
+
         let servers = data['4fun'].servers
         let forFunArea = document.querySelector('.forfun-area');
         let navigationArea = document.querySelector('.navigation-area')
         let countServers = 1;
         let countGroup = 1;
+        let idCopy = 1;
+
+        servers.forEach(element => {
+
+            // <define the width size of progress bar server>
+            const maxServer = element.max;
+            const currentServer = element.current;
+            const sizeProgressBar = (currentServer * 101) / maxServer
+            // </define the width size of progress bar server>
+
+            if (countServers == 1) {
+
+                const navigationButton = `
+                    <label class="navigation-forfun">
+                        <input type="radio" class="reference-${countGroup}" name="forfun" id="navigation-${countGroup}" ${countGroup == 1 ? 'checked=checked' : ''}">
+                        <span></span>
+                    </label>
+                `
+
+                navigationArea.insertAdjacentHTML('beforeend', navigationButton);
+
+                const groupServersArea = `
+                    <div class="group-quad-area reference-${countGroup} ${countGroup == 1 ? 'show':'hide'}" id="group-quad-${countGroup}">
+                        
+                    </div>
+                `
+                forFunArea.insertAdjacentHTML('beforeend', groupServersArea);
+            }
+
+            const groupServersArea = `
+                <article class="forfun-quad">
+                    <div class="flex-area w-100">
+                        <button class="bt-forfun-area -btn-clipboard" data-clipboard-id="${idCopy}" data-clipboard-text="${element.copy}">
+                            <img src="img/copy-paste-icon.png" alt="Gamers Club">
+                        </button>
+                        <a href="${element.join}" class="bt-forfun-area -green"> <img src="img/goto-icon.png" alt="Gamers Club">
+                        </a>
+                    </div>
+
+                    <div class="w-100">
+                        <div>
+                            <p class="descri-forfun-area">#${element.id} - Deathmatch <strong>${element.mode}</strong></p>
+                        </div>
+                        <div>
+                            <p class="title-progressbar-forfun-area"><span class="map-name">${element.map}</span> <span>${element.current}/${element.max}</span></p>
+                            <div class="progress-bar-forfun-area">
+                                <span class="progress-bar-forfun" style="width:${sizeProgressBar}%;"></span>
+                            </div>
+                        </div>
+                    </div>
+                </article> 
+            `
+
+            //<Make Copy button works>
+            new ClipboardJS(`[data-clipboard-id="${idCopy}"]`);
+            //</Make Copy button works>
+
+            let groupArea = document.querySelector(`#group-quad-${countGroup}`);
+            groupArea.insertAdjacentHTML('beforeend', groupServersArea);
+
+            countServers++
+
+            if (countServers == 4) {
+                countServers = 1;
+                countGroup++;
+            }
+
+        })
+
+        listenNavigation()
+    }
+
+
+    function buildLobbyAndRankedArea(data) {
 
         let lobbyTitle = document.querySelector('.title-lobby-area')
         let RankedTitle = document.querySelector('.title-ranked-area')
@@ -112,103 +199,41 @@
         buttonLobby.href = data.games[0].cta.link
         buttonRanked.href = data.games[1].cta.link
         buttonRankedNumber.textContent = data.games[1].cta.line
+    }
 
+    function buildfooter(data) {
+        let onlinePlayers = document.querySelector('.online-players')
+        let bannedPlayers = document.querySelector('.banned-players')
 
-
-        servers.forEach( element => {
-
-            // <define the width size of progress bar server>
-            const maxServer = element.max;
-            const currentServer = element.current;
-            const sizeProgressBar = (currentServer * 101) / maxServer
-            // </define the width size of progress bar server>
-
-            if (countServers == 1) {
-
-                const navigationButton = `
-                    <label class="navigation-forfun">
-                        <input type="radio" class="reference-${countGroup}" name="forfun" id="navigation-${countGroup}" ${countGroup == 1 ? 'checked=checked' : ''}">
-                        <span></span>
-                    </label>
-                `
-
-                navigationArea.insertAdjacentHTML('beforeend', navigationButton);
-
-                const groupServersArea = `
-                    <div class="group-quad-area reference-${countGroup} ${countGroup == 1 ? 'show':'hide'}" id="group-quad-${countGroup}">
-                        
-                    </div>
-                `
-                forFunArea.insertAdjacentHTML('beforeend', groupServersArea);
-            }
-
-            const groupServersArea = `
-                <article class="forfun-quad">
-                    <div class="flex-area w-100">
-                        <button class="bt-forfun-area -btn-clipboard" data-clipboard-text="${element.copy}">
-                            <img src="img/copy-paste-icon.png" alt="Gamers Club">
-                        </button>
-                        <a href="${element.join}" class="bt-forfun-area -green"> <img src="img/goto-icon.png" alt="Gamers Club">
-                        </a>
-                    </div>
-
-                    <div class="w-100">
-                        <div>
-                            <p class="descri-forfun-area">#${element.id} - Deathmatch <strong>${element.mode}</strong></p>
-                        </div>
-                        <div>
-                            <p class="title-progressbar-forfun-area"><span class="map-name">${element.map}</span> <span>${element.current}/${element.max}</span></p>
-                            <div class="progress-bar-forfun-area">
-                                <span class="progress-bar-forfun" style="width:${sizeProgressBar}%;"></span>
-                            </div>
-                        </div>
-                    </div>
-                </article> 
-            `
-
-            new ClipboardJS('.-btn-clipboard');
-
-            let groupArea = document.querySelector(`#group-quad-${countGroup}`);
-            groupArea.insertAdjacentHTML('beforeend', groupServersArea);
-
-            countServers++
-
-            if (countServers == 4) {
-                countServers = 1;
-                countGroup++;
-            }
-
-        })
-        
-        listenNavigation()
+        onlinePlayers.textContent = data.online
+        bannedPlayers.textContent = data.latest_banned
     }
 
 })();
 
 
-
-function listenNavigation(){
+function listenNavigation() {
 
     let allCheckedButtons = document.querySelectorAll('input[type="radio"]');
 
-        allCheckedButtons.forEach(element => {
-            element.addEventListener('click', (event)=>{
-                let clickedNavigation = event.target
-          
-                if(clickedNavigation.checked){
-                    let clickedNavigationReference = clickedNavigation.classList[0];
-                    let allGroups = document.querySelectorAll('.group-quad-area');
+    allCheckedButtons.forEach(element => {
+        element.addEventListener('click', (event) => {
+            let clickedNavigation = event.target
 
-                    allGroups.forEach((element)=>{
-                        if(element.classList.contains(`${clickedNavigationReference}`)){
-                            element.classList.add('show')
-                            element.classList.remove('hide')
-                        }else{
-                            element.classList.add('hide')
-                            element.classList.remove('show')
-                        }
-                    })
-                }
-            })
+            if (clickedNavigation.checked) {
+                let clickedNavigationReference = clickedNavigation.classList[0];
+                let allGroups = document.querySelectorAll('.group-quad-area');
+
+                allGroups.forEach((element) => {
+                    if (element.classList.contains(`${clickedNavigationReference}`)) {
+                        element.classList.add('show')
+                        element.classList.remove('hide')
+                    } else {
+                        element.classList.add('hide')
+                        element.classList.remove('show')
+                    }
+                })
+            }
         })
+    })
 }
